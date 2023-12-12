4316de119b64b8ba74fd5a887d42ba2017d81cc9
Joshua <64817207+Joshua-desings@users.noreply.github.com>
2023-12-12 19:59:14 -0400

    Nuevas funcionalidades manejo de secciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // main.js
-  
 document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
     const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
 
+    // Cargar las canciones en alza al iniciar la página
     fetch(`${apiUrl}?method=chart.getTopTracks&api_key=${apiKey}&format=json&limit=8&period=year`)
         .then(response => response.json())
         .then(data => {
@@ -33,8 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                         const image = document.createElement("img");
                         image.classList.add("chart-image");
-                        // Verificar si la URL de la imagen está disponible
-                        image.src = trackInfo.track.album.image[2]['#text'] || 'placeholder.jpg'; // Puedes usar una imagen de marcador de posición
+                        image.src = trackInfo.track.album.image[2]['#text'] || 'placeholder.jpg';
                         image.alt = track.name;
 
                         bar.appendChild(label);
@@ -49,14 +48,12 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error al obtener datos de la API (Temas en alza):', error));
 
-
     // Ejemplo de cómo realizar una solicitud a la API de Last.fm para obtener información de artistas
     const artistSection = document.getElementById('artistas');
 
     fetch(`${apiUrl}?method=artist.getInfo&artist=Coldplay&api_key=${apiKey}&format=json`)
         .then(response => response.json())
         .then(data => {
-            // Manejar la respuesta de la API y actualizar el contenido en la sección de artistas
             artistSection.innerHTML = `<h2>${data.artist.name}</h2>
                                        <p>${data.artist.bio.summary}</p>`;
         })
@@ -88,7 +85,7 @@ function drawChart(songNames, chartData) {
 function createChartBar(popularity, label, track) {
     const bar = document.createElement("div");
     bar.classList.add("chart-bar");
-    bar.style.height = `${(popularity / Math.max(...chartData)) * 100}%`; // Ajuste para la altura de las barras
+    bar.style.height = `${(popularity / Math.max(...chartData)) * 100}%`;
 
     const chartLabel = document.createElement("div");
     chartLabel.classList.add("chart-label");
@@ -97,11 +94,9 @@ function createChartBar(popularity, label, track) {
     const image = document.createElement("img");
     image.classList.add("chart-image");
 
-    // Verificamos si 'track' está definido y si tiene la propiedad 'image'
     if (track && track.image && Array.isArray(track.image) && track.image.length > 2) {
         image.src = track.image[2]['#text'];
     } else {
-        // Si no hay imagen disponible, puedes mostrar una imagen de reemplazo o dejarlo vacío
         image.src = '../images/Placeholder.png';
     }
 
@@ -113,38 +108,31 @@ function createChartBar(popularity, label, track) {
     return bar;
 }
 
+// Cargar los artistas principales al iniciar la página
 document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
     const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
 
-    // Obtener los datos de los artistas principales
     fetch(`${apiUrl}?method=chart.getTopArtists&api_key=${apiKey}&format=json&limit=5`)
         .then(response => response.json())
         .then(data => {
             const artistCarousel = document.getElementById("artistCarousel");
 
-            // Crear elementos HTML para cada artista en el carrusel
             data.artists.artist.forEach(artist => {
                 createArtistCard(artist, apiKey, apiUrl)
                     .then(artistCard => {
-                        // Agregar la tarjeta del artista al carrusel
                         artistCarousel.appendChild(artistCard);
                     })
                     .catch(error => console.error('Error al crear la tarjeta del artista:', error));
             });
         })
         .catch(error => console.error('Error al obtener datos de la API (Top Artistas):', error));
-
-    // Resto del código relacionado con la API de Last.fm (variables apiUrl, apiKey, etc.)
 });
 
+// Crear un contenedor para los videos de YouTube
 document.addEventListener("DOMContentLoaded", function () {
-    // ... (tu código existente)
-
-    // Crear un contenedor para los videos de YouTube
     const youtubeContainer = document.getElementById("youtubeContainer");
 
-    // Agregar videos de YouTube al contenedor
     const youtubeVideos = [
         "Y4M6Nm5UQOU",
         // Agrega más IDs de videos según sea necesario
@@ -153,30 +141,26 @@ document.addEventListener("DOMContentLoaded", function () {
     youtubeVideos.forEach(videoId => {
         const youtubeEmbed = document.createElement("iframe");
         youtubeEmbed.src = `https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1`;
-        youtubeEmbed.width = 510; // Ajusta el tamaño según tus preferencias
-        youtubeEmbed.height = 325; // Ajusta el tamaño según tus preferencias
+        youtubeEmbed.width = 510;
+        youtubeEmbed.height = 325;
         youtubeEmbed.allowFullscreen = true;
         youtubeContainer.appendChild(youtubeEmbed);
     });
-
-    // ... (resto de tu código existente)
 });
 
+// Cargar más artistas al iniciar la página
 document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
     const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
 
-    // Obtener los datos de los artistas principales
     fetch(`${apiUrl}?method=chart.getTopArtists&api_key=${apiKey}&format=json&limit=10`)
         .then(response => response.json())
         .then(data => {
             const artistCarousel = document.getElementById("artistCarousel");
 
-            // Crear elementos HTML para cada artista en el carrusel
             data.artists.artist.forEach(artist => {
                 createArtistCard(artist, apiKey, apiUrl, artistCarousel)
                     .then(artistCard => {
-                        // Agregar la tarjeta del artista al carrusel
                         artistCarousel.appendChild(artistCard);
                     })
                     .catch(error => console.error('Error al crear la tarjeta del artista:', error));
@@ -192,21 +176,17 @@ async function createArtistCard(artist, apiKey, apiUrl, artistCarousel) {
         artistCard.classList.add("artist-card");
 
         try {
-            // Obtener los álbumes más populares del artista
             const topAlbumsResponse = await fetch(`${apiUrl}?method=artist.getTopAlbums&api_key=${apiKey}&artist=${encodeURIComponent(artist.name)}&format=json&limit=1`);
             const topAlbumsData = await topAlbumsResponse.json();
 
-            // Verificar si ya se ha agregado este álbum
             if (!isAlbumDuplicate(topAlbumsData.topalbums.album, artistCarousel)) {
-                // Extraer la imagen del artista desde uno de los álbumes
                 const image = document.createElement("img");
-                const imageUrl = topAlbumsData.topalbums.album[0].image[3]['#text']; // Utilizamos la imagen de mayor calidad disponible
+                const imageUrl = topAlbumsData.topalbums.album[0].image[3]['#text'];
 
                 if (imageUrl) {
                     image.src = imageUrl;
                     image.alt = artist.name;
                 } else {
-                    // Si la URL de la imagen no está disponible, puedes usar una imagen de marcador de posición
                     image.src = 'placeholder.jpg';
                     image.alt = 'Placeholder';
                 }
@@ -219,7 +199,6 @@ async function createArtistCard(artist, apiKey, apiUrl, artistCarousel) {
 
                 resolve(artistCard);
             } else {
-                // Si el álbum ya está en el carrusel, resolvemos con null para indicar que no se debe agregar
                 resolve(null);
             }
         } catch (error) {
@@ -234,6 +213,246 @@ function isAlbumDuplicate(albumData, artistCarousel) {
     return albumNames.has(albumData[0].name);
 }
 
+// Función para cargar los generos en la seccion musica
+async function loadMusicGenres() {
+    const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+    const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
+
+    try {
+        const response = await fetch(`${apiUrl}?method=tag.getTopTags&api_key=${apiKey}&format=json`);
+        const data = await response.json();
+
+        const genresContainer = document.getElementById('musicGenresContainer');
+        genresContainer.innerHTML = '';
+
+        const genresList = document.createElement("ul");
+        genresList.classList.add("genres-list");
+
+        data.toptags.tag.forEach(tag => {
+            const genreItem = document.createElement("li");
+            genreItem.classList.add("genre-item");
+
+            const genreImage = document.createElement("img");
+            genreImage.src = getGenreImage(tag) || 'images/placeholder.png';
+            genreImage.alt = tag.name;
+
+            const genreName = document.createElement("p");
+            genreName.innerText = tag.name;
+
+            genreItem.appendChild(genreImage);
+            genreItem.appendChild(genreName);
+
+            genreItem.addEventListener("click", () => {
+                console.log(`Género seleccionado: ${tag.name}`);
+            });
+
+            genresList.appendChild(genreItem);
+        });
+
+        genresContainer.appendChild(genresList);
+    } catch (error) {
+        console.error('Error al obtener datos de la API (Géneros musicales):', error);
+    }
+}
+
+function getGenreImage(tag) {
+    // Verificar si hay imágenes disponibles en la propiedad "image"
+    if (tag.image && Array.isArray(tag.image) && tag.image.length > 0) {
+        // Obtener la última imagen disponible (cambia el índice si es necesario)
+        const lastImageIndex = tag.image.length - 1;
+        return tag.image[lastImageIndex]['#text'];
+    }
+
+    // Si no hay imágenes disponibles, devolver null
+    return null;
+}
+
+function search() {
+    const searchTerm = document.getElementById('searchInput').value;
+
+    // Actualizar la URL con los parámetros de búsqueda
+    const newUrl = `${window.location.origin}${window.location.pathname}#searchResults?q=${encodeURIComponent(searchTerm)}`;
+    window.location.href = newUrl;
+
+    // Llamar a funciones para obtener y mostrar resultados
+    searchArtists(searchTerm);
+    searchAlbums(searchTerm);
+    searchTracks(searchTerm);
+
+    document.getElementById('searchResults').style.display = 'block';
+}
+
+// Función para buscar artistas
+async function searchArtists(searchTerm) {
+    const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+    const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
+
+    try {
+        const response = await fetch(`${apiUrl}?method=artist.search&artist=${encodeURIComponent(searchTerm)}&api_key=${apiKey}&format=json`);
+        const data = await response.json();
+        const artists = data.results.artistmatches.artist;
+
+        // Obtener imágenes para artistas
+        const artistImages = await Promise.all(artists.map(async artist => {
+            try {
+                const artistInfoResponse = await fetch(`${apiUrl}?method=artist.getinfo&artist=${encodeURIComponent(artist.name)}&api_key=${apiKey}&format=json`);
+                const artistInfoData = await artistInfoResponse.json();
+
+                // Verificar si hay imágenes del artista disponibles
+                const artistImages = artistInfoData.artist.image;
+                if (artistImages && artistImages.length > 0) {
+                    // Devolver la última imagen disponible (generalmente la de mayor calidad)
+                    return artistImages[artistImages.length - 1]['#text'] || '';
+                } else {
+                    console.warn('No se encontraron imágenes para el artista:', artist.name);
+                    return 'placeholder.jpg'; // Usar imagen de marcador de posición si no hay imágenes disponibles
+                }
+            } catch (error) {
+                console.error('Error al obtener información del artista:', error);
+                return 'placeholder.jpg'; // Usar imagen de marcador de posición si hay un error
+            }
+        }));
+
+        displayResults(artists, 'artistsResults', 8, 'Resultados de Artistas', artistImages);
+    } catch (error) {
+        console.error('Error al buscar artistas:', error);
+    }
+}
+
+// Función para buscar álbumes
+async function searchAlbums(searchTerm) {
+    const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+    const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
+
+    try {
+        const response = await fetch(`${apiUrl}?method=album.search&album=${encodeURIComponent(searchTerm)}&api_key=${apiKey}&format=json`);
+        const data = await response.json();
+        const albums = data.results.albummatches.album;
+
+        // Obtener imágenes para álbumes
+        const albumImages = await Promise.all(albums.map(async album => {
+            const albumInfo = await fetch(`${apiUrl}?method=album.getinfo&artist=${encodeURIComponent(album.artist)}&album=${encodeURIComponent(album.name)}&api_key=${apiKey}&format=json`);
+            const albumData = await albumInfo.json();
+            return albumData.album.image[2]['#text']; // Usamos la tercera imagen (de tamaño medio)
+        }));
+
+        displayResults(albums, 'albumsResults', 8, 'Resultados de Álbumes', albumImages);
+    } catch (error) {
+        console.error('Error al buscar álbumes:', error);
+    }
+}
+
+// Función para buscar pistas
+async function searchTracks(searchTerm) {
+    const apiUrl = 'http://ws.audioscrobbler.com/2.0/';
+    const apiKey = 'f04f441ada53ca5ad2085e02660c2406';
+
+    try {
+        const response = await fetch(`${apiUrl}?method=track.search&track=${encodeURIComponent(searchTerm)}&api_key=${apiKey}&format=json`);
+        const data = await response.json();
+        const tracks = data.results.trackmatches.track;
+
+        // Obtener imágenes para pistas
+        const trackImages = await Promise.all(tracks.map(async track => {
+            try {
+                const trackInfoResponse = await fetch(`${apiUrl}?method=track.getinfo&artist=${encodeURIComponent(track.artist)}&track=${encodeURIComponent(track.name)}&api_key=${apiKey}&format=json`);
+                const trackInfoData = await trackInfoResponse.json();
+                const albumImage = trackInfoData.track.album.image[2]['#text']; // Intentamos obtener la imagen del álbum
+
+                // Verificar si la imagen del álbum existe, de lo contrario, intentamos obtener la imagen del artista
+                return albumImage || getArtistImage(track.artist, apiKey);
+            } catch (error) {
+                console.error('Error al obtener información de la pista:', error);
+                return ''; // Devolver cadena vacía si hay un error
+            }
+        }));
+
+        displayResults(tracks, 'tracksResults', 12, 'Resultados de Pistas', trackImages);
+    } catch (error) {
+        console.error('Error al buscar pistas:', error);
+    }
+}
+// Función genérica para mostrar resultados en un contenedor específico
+function displayResults(results, containerId, limit, subtitle, images) {
+    const resultsContainer = document.getElementById(containerId);
+    resultsContainer.innerHTML = '';
+
+    if (results.length > 0) {
+        // Agregar subtítulo solo si es necesario
+        if (subtitle) {
+            const subtitleElement = document.createElement('h2');
+            subtitleElement.textContent = subtitle;
+            resultsContainer.appendChild(subtitleElement);
+        }
+
+        // Limitar la cantidad de resultados
+        results = results.slice(0, limit);
+
+        const resultsWrapper = document.createElement('div');
+        resultsWrapper.classList.add('results-wrapper');
+
+        results.forEach((result, index) => {
+            const resultItem = document.createElement('div');
+            resultItem.classList.add('result-item');
+
+            // Personalizar según la estructura de los resultados
+            const resultName = document.createElement('h3');
+            resultName.textContent = result.name;
+
+            // Agregar imagen solo si existe
+            if (images && images[index]) {
+                const resultImage = document.createElement('img');
+                resultImage.src = images[index]; // Asociar la imagen correspondiente
+                resultImage.alt = result.name;
+                resultItem.appendChild(resultImage);
+            }
+
+           // Verificar si es la sección de artistas para evitar mostrar información adicional
+if (containerId === 'artistsResults') {
+    // Mostrar solo el nombre del artista
+    resultItem.appendChild(resultName);
+} else {
+    // Agregar más información según sea necesario
+    const resultInfo = document.createElement('p');
+
+    // Mostrar información específica según la sección
+    if (containerId === 'tracksResults') {
+        resultInfo.textContent = ` ${result.name || ''}`;
+    } else if (containerId === 'albumsResults') {
+        resultInfo.textContent = ` ${result.name || ''}`;
+        resultItem.appendChild(resultInfo);
+
+        // Agregar el nombre del artista debajo del nombre del álbum
+        const artistInfo = document.createElement('p');
+        artistInfo.classList.add('artist-info'); // Agregar la clase 'artist-info'
+        artistInfo.textContent = ` ${result.artist || ''}`;
+        resultItem.appendChild(artistInfo);
+    } else {
+        // Otro caso, si es necesario agregar información adicional
+        resultInfo.textContent = ` ${result.artist || ''}`;
+        resultItem.appendChild(resultInfo);
+    }
+                resultItem.appendChild(resultInfo);
+            }
+
+            resultsWrapper.appendChild(resultItem);
+        });
+
+        resultsContainer.appendChild(resultsWrapper);
+    } else {
+        resultsContainer.textContent = 'No se encontraron resultados';
+    }
+    // Función para manejar la búsqueda al presionar Enter
+function handleEnterKey(event) {
+    if (event.key === 'Enter') {
+        search();
+    }
+}
+
+// Agregar el evento para manejar la tecla Enter al campo de búsqueda
+document.getElementById('searchInput').addEventListener('keyup', handleEnterKey);
+}
 
 
-// Resto del código relacionado con la API de Last.fm (variables apiUrl, apiKey, etc.)
+
+
